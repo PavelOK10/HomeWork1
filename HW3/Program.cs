8490c9b73b9dc08a79dd44fe8ee3de75b388a13c
PaRus <PaRusakovich@gmail.com>
2021-04-23 19:33:50 +0300

    Добавьте файлы проекта.

---
+++ HW3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Dollar = 2.66;
+            double Euro = 3.55;
+            Console.WriteLine("Введите сумму рублей");
+            double rouble = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cумма в долларах: " + rouble / Dollar);
+            Console.WriteLine("Cумма в евро: " + rouble / Euro);
+            
+        }
+    }
+}
---
+++ Home/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВедите первое число ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье число");
+            double c = double.Parse(Console.ReadLine());
+            double d = a + b + c;
+            double e = a * b * c;
+
+            Console.WriteLine("Сумма " + d);
+            Console.WriteLine("Произведение" + e);
+        }
+    }
+}
